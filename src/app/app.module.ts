f72c9e971da656fab36f32e9e5065001aaade49a
Amel TRIKI <amel.triki.iset@gmail.com>
2021-11-24 17:49:11 +0100

    Projets avec composants, classes et services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { ListlivresComponent } from './components/listlivres/listlivres.component';
+import { SelectedLivreComponent } from './components/selected-livre/selected-livre.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PresentationComponent,
+    ListlivresComponent,
+    SelectedLivreComponent,
+    AboutusComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/selected-livre/selected-livre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-livre',
+  templateUrl: './selected-livre.component.html',
+  styleUrls: ['./selected-livre.component.css']
+})
+export class SelectedLivreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
